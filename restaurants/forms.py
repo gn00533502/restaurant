48b1f4324a754a119a47f68c7b469e3c0960faad
gn00533502 <gn00533502@gmail.com>
2015-11-10 23:54:35 +0800

    forms.py調整自定義驗證規則

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -6,3 +6,10 @@ class CommentForm(forms.Form):
     visitor = forms.CharField(max_length = 20)
     email = forms.EmailField(max_length = 20, required = False) #required = False表示並非必填
     content = forms.CharField(max_length = 200, widget = forms.Textarea())
+    
+    #自定義驗證規則
+    def clean_content(self):
+    	content = self.cleaned_data['content']
+    	if len(content) < 5:
+    		raise forms.ValidationError('字數不足')
+    	return content
