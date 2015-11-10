8b210fbd2d7d435614823340721efcc914216c11
gn00533502 <gn00533502@gmail.com>
2015-11-10 20:58:46 +0800

    表單的綁定與驗證調整

--- restaurants/views.py
+++ restaurants/views.py
@@ -37,28 +37,20 @@ def comment(request, restaurant_id):
         return HttpResponseRedirect("/restaurants_list/")
     errors = [] #用於作欄位正確性判斷
     if request.POST:
-        visitor = request.POST['visitor']
-        content = request.POST['content']
-        email = request.POST['email']
-        date_time = timezone.localtime(timezone.now())
-        #當欄位未填，放入error(這時候error變False)
-        if any(not request.POST[k] for k in request.POST):
-            errors.append('* 有空白欄位，請不要留空')
-
-        #檢驗email
-        if '@' not in email:
-            errors.append('* email格式有誤，請重新輸入')
-
-        #若不是空白或檢驗無誤
-        if not errors:
-            Comment.objects.create(
+        f = CommentForm(request.POST)
+        if f.is_valid():
+            visitor = f.cleaned.data['visitor']
+            content = f.cleaned.data['content']
+            email = f.cleaned.data['email']
+            date_time = timezone.localtime(timezone.now())
+            c = Comment.objects.create(
                 visitor=visitor, email=email,
                 content=content,
                 date_time=date_time,
                 restaurant=r
             )
-            #填寫成功將欄位清空
-            visitor, email, content = ("","","")
-    #產生一個非綁定表單，將生成html碼
-    f = CommentForm()
+            #產生一個非綁定表單，將生成html碼
+            f = CommentForm()
+    else:
+        f = CommentForm()
     return render_to_response('comments.html', RequestContext(request,locals()))
